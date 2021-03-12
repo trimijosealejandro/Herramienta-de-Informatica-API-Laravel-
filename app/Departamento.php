35b0f92580c537cec7e064e6417a18c30c370b8b
trimijosealejandro93 <trimijosealejandro@yahoo.com>
2021-03-12 10:55:47 -0430

    Creado la tabla departamento con todo sus metodos

---
+++ app/Departamento.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    protected $table='departamentos';
+    protected $fillable=[
+        'name',
+    ];
+    /* public function pc()
+    {
+        return $this->hasMany('App\Pc','departamentos_id');
+    } */
+}
---
+++ app/Http/Controllers/DepartamentoController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DepartamentoController extends Controller
+{
+   //GET Retorna una lista de la tabla departamentos pasndo el filtro por parametros
+    public function index(Request $request)
+    {
+        if($request->has('txtBuscar')){
+            $departamento=Departamento::where('name','like','%'.$request->txtBuscar.'%')
+            ->get();
+        }else{
+            $departamento=Departamento::all();
+        }
+        return $departamento;
+    }
+
+    //POST adiciona un registro a la tabla departamentos
+    public function store(Request $request)
+    {
+        $data=$request->all();
+        Departamento::create($data);
+
+        return response()->json([
+            'res'=>true,
+            'mesagge'=>'Departamento creado correctamente'
+        ],200);
+    }
+
+    //GET Muestra un registro de la tabla departamento enviado por parametro
+    public function show(Departamento $departamento)
+    {
+        return $departamento;
+    }
+
+    //PUT Actualiza un registro con los datos enviados por parametro
+    public function update(Request $request, Departamento $departamento)
+    {
+        $data=>$request()->all();
+        $departamento->update($data);
+        return response()->json([
+            'res'=>true,
+            'mesagge'=>'Departamento actualizado correctamente'
+        ],200);
+    }
+
+    //DELETE elimina un registro de la tabla departamento
+    public function destroy($id)
+    {
+        Departamento::destroy($id);
+        return return response()->json([
+            'res'=>true,
+            'mesagge'=>'Departamento eliminado correctamente'
+        ],200);
+    }
+}
